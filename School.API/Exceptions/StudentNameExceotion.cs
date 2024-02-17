472f793cbe1eb916e7f8e4cd2529aa286d74b5f5
Anton Magdy <anton.magdy@hos.asu.edu.eg>
2024-02-17 13:29:54 +0200

    Finished third video in secound chapter

---
+++ School.API/Exceptions/StudentNameExceotion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace School.API.Exceptions
+{
+    public class StudentNameExceotion:Exception
+    {
+        public string StudentName { get; set; }
+        public StudentNameExceotion(string message):base (message) 
+        {
+        
+        }
+        public StudentNameExceotion(string message ,Exception innerException):base (message, innerException) 
+        {
+
+        }
+        public StudentNameExceotion(string message, string studentName) : base(message)
+        {
+            StudentName = studentName;
+        }
+    }
+}
